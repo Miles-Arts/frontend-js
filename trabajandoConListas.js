75394279c75ab3c3c3b81cde079f9e800686f4c8
Milton Figueredo Miles <gratno@gmail.com>
2022-10-12 16:51:56 -0500

    Arrays

---
+++ trabajandoConListas.js
@@ -0,0 +1,30 @@
+console.log(`Trabajando con listas`);
+
+const bogota = "Bogotá";
+const lima = "Lima";
+const santiago = "Santiago";
+const buenosAires = "Buenos Aires";
+
+//console.log(bogota,lima,santiago,buenosAires);
+
+//const ciudadesDisponibles = new Array ();
+
+//const ciudadesDisponibles = [`Bogotá` , `Lima` , `Santiago` , `Bueno Aires`];
+
+const ciudadesDisponibles = new Array();
+
+ciudadesDisponibles.push("Bogotá");
+ciudadesDisponibles.push("Caracas");
+ciudadesDisponibles.push("Lima");
+
+console.log(ciudadesDisponibles);
+
+const ciudadesMasVendidas = [];
+
+
+
+ciudadesMasVendidas.push("Sáo Paulo");
+ciudadesMasVendidas.push("Buenos Aires");
+ciudadesMasVendidas.push("Tigre");
+
+console.log(ciudadesMasVendidas);
