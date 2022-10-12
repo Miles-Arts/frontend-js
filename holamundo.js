343daac436d817ee869ec5710668625d7980cb1b
Milton Figueredo Miles <gratno@gmail.com>
2022-10-12 11:22:00 -0500

    Probando JS

--- holamundo.js
+++ holamundo.js
@@ -1,2 +1,13 @@
-console.log ("mi primer programa hahah");
-console.log (2+2);
+console.log("mi primer programa hahah");
+console.log(2 + 2);
+
+
+const nombre = "Margarita ";
+const edad = 38;
+const nombreMadre = " Luna";
+const nombrePadre = " y Paco";
+const nombreHijos = " Missi y Negrito";
+const edadMadre = 36;
+
+console.log("Me llamo " + nombre + " mi edad es " + edad + ". mi mamá y papá se llaman " + nombreMadre + nombrePadre + " y mis hermanos se llaman " + nombreHijos);
+console.log ("La edad de mi mamá es: " + edadMadre + " añitos.");
