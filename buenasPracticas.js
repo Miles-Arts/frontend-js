05b023dbd6e9ca70df7b2447cfa931b4794cded7
Milton Figueredo Miles <gratno@gmail.com>
2022-10-22 18:36:48 -0500

    Cambiar variables

--- buenasPracticas.js
+++ buenasPracticas.js
@@ -1,5 +1,11 @@
 let nombreEmpleado = "José Perez ";
 let NombreEmpleado = "Pedro Martins";
 
+let edadEmpleado = 38;
+
 console.log(nombreEmpleado);
 console.log(NombreEmpleado); //JS es sensible a mn´puscula y mayúsculas
+console.log(edadEmpleado);
+
+edadEmpleado = "Treinta y Nueve";
+console.log(edadEmpleado);
---
+++ conversionDeTipos.js
@@ -0,0 +1,19 @@
+//Tipos de datos primitivos
+//Alfa númerico
+
+const nombrePasajero = "Luisa España";
+console.log(nombrePasajero); //Las CONST no se pueden cambiar en el tiempo
+
+
+let nombrePasajera = "Romina Medina";
+console.log("Nombre clienta: " + nombrePasajera);
+
+nombrePasajera = "Juanita";
+console.log("Nombre nueva clienta: " + nombrePasajera + " - misma variable distinto nombre XD");
+
+
+//Dato númerico
+
+const valorBoleto = 1300;
+
+console.log("Valor pasaje: " + valorBoleto);
