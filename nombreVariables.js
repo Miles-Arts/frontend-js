0496c9fb5a04a4b5d13c9af169cd4a93d06d19cb
Milton Figueredo Miles <gratno@gmail.com>
2022-10-22 18:07:46 -0500

    Probando variables

--- nombreVariables.js
+++ nombreVariables.js
@@ -4,9 +4,14 @@ var profesion = " Arquitecta";
 var ciudadVivienda = " Tunja";
 let direccion = " Avenida 6 Km 2 vía Paipa. Conjunto Rosal Norte"
 const nacionalidad = " Colombia";
-const tipoSangre = " o+";
+const tipoSangre = " O+";
 
-console.log(nombre, edad, profesion, ciudadVivienda, direccion, nacionalidad, tipoSangre);
+console.log("Nombre: " + nombre);
+console.log("Edad:  " + edad);
+console.log("Tipo de sangre: " + tipoSangre);
+console.log("Profesión:  " + profesion);
+console.log("Ciudad: " + ciudadVivienda);
+console.log("Dirección: " + direccion);
 
 //const miNombre = "Leonardo";
 //console.log("miNombre");
