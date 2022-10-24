3543d974f834ea978a00475b175c85b87dd0b129
Milton Figueredo Miles <gratno@gmail.com>
2022-10-24 14:06:00 -0500

    Ejercicioe Array

---
+++ listasArray.js
@@ -0,0 +1,29 @@
+//Array ciudad1 = "Bogotá";
+
+
+//Definición de una lista, con tipo de Dato alfa númerico
+
+const ciudadesDisponibles = new Array("Bogotá", "Lima", "Santiago");
+
+//console.log(ciudadesDisponibles);
+//console.log(ciudadesDisponibles("1"));
+
+//Definición de una lista de forma abreviada
+
+const paisesDisponibles = ["Colombia", "Perú", "Chile", "Brasil", "Argentina"];
+//console.log(paisesDisponibles);
+
+
+    paisesDisponibles.push("Uruguay");
+    ciudadesDisponibles.push("Montevideo");
+
+    //console.log(ciudadesDisponibles);
+    //console.log(paisesDisponibles);
+
+
+
+    paisesDisponibles.unshift("Ecuador");
+    ciudadesDisponibles.unshift("Quito//");
+
+    console.log(ciudadesDisponibles);
+    console.log(paisesDisponibles);
