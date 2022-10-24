29b1af48ac89987079e8717d5416f5408cfb00bc
Milton Figueredo Miles <gratno@gmail.com>
2022-10-24 13:15:41 -0500

    Ejercicio de clase Array

---
+++ plantillasDeTexto.js
@@ -0,0 +1,16 @@
+//Template String
+//INTERPoLAR VARIABLES, es sustituir variables 
+
+const nombrePasajero = "Luisa";
+const apellidoPasajero = "Mondragon";
+
+let  nombreCompleto = `El nombre completo es: ${nombrePasajero} ${apellidoPasajero}`;
+
+console.log(nombreCompleto);
+
+let segundoPasajero = "José Castro";
+
+console.log(`El  nombre del segundo pasajero es: ${segundoPasajero}`);
+
+segundoPasajero = 123456;
+console.log(`El  nombre del segundo pasajero es: ${segundoPasajero}`);
