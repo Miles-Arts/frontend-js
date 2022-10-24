7d33416ee04a80d1c38a504783e945e05ace6c3f
Milton Figueredo Miles <gratno@gmail.com>
2022-10-24 14:17:43 -0500

    Ejercicio Array

--- listasArray.js
+++ listasArray.js
@@ -6,24 +6,26 @@
 const ciudadesDisponibles = new Array("Bogotá", "Lima", "Santiago");
 
 //console.log(ciudadesDisponibles);
-//console.log(ciudadesDisponibles("1"));
-
+//console.log(ciudadesDisponibles("1"))
 //Definición de una lista de forma abreviada
-
 const paisesDisponibles = ["Colombia", "Perú", "Chile", "Brasil", "Argentina"];
 //console.log(paisesDisponibles);
 
+paisesDisponibles.push("Uruguay");
+ciudadesDisponibles.push("Montevideo");
 
-    paisesDisponibles.push("Uruguay");
-    ciudadesDisponibles.push("Montevideo");
+//console.log(ciudadesDisponibles);
+//console.log(paisesDisponibles);
 
-    //console.log(ciudadesDisponibles);
-    //console.log(paisesDisponibles);
+paisesDisponibles.unshift("Ecuador");
+ciudadesDisponibles.unshift("Quito");
 
+//console.log(ciudadesDisponibles);
+//console.log(paisesDisponibles);
 
+//Mostrando el primer elemento de la lista
+console.log(paisesDisponibles[0]);
+console.log(ciudadesDisponibles[0]);
 
-    paisesDisponibles.unshift("Ecuador");
-    ciudadesDisponibles.unshift("Quito//");
-
-    console.log(ciudadesDisponibles);
-    console.log(paisesDisponibles);
+paisesDisponibles.splice(1,2,"Venezuela", "Paraguay");
+console.log(paisesDisponibles);
