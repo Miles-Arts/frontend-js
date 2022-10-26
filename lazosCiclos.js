cafa5988f25e907e76de11665d4d49bc55f057cf
Milton Figueredo Miles <gratno@gmail.com>
2022-10-26 13:34:55 -0500

    While

--- lazosCiclos.js
+++ lazosCiclos.js
@@ -1,12 +1,18 @@
 const ciudadDestino = "Quito";
 const ciudadesDisponibles = new Array("Bogotá", "Lima", "Santiago", "Montevideo", "Caracas", "Buenos Aires", "Santa Cruz");
-const precioCiudad = new Array(500, 400, 380, 200, 390);
-const presupuestoDisponible = 390;
+const precioCiudad = new Array(500, 400, 380, 200, 390, 389, 350);
+const presupuestoDisponible = 210;
 
 let i = 0;
 
-//console.log(`Verificando pasajes para ${ciudadDestino}`);
+//While  0 o más veces
+//Do while 1 o más veces
+//
 
-while (precioCiudad[i] > presupuestoDisponible) {
+
+while (precioCiudad[i] > presupuestoDisponible && i < ciudadesDisponibles.length) {
     i++;
-} console.log("Puedes comprar pasaje para: " + ciudadesDisponibles[i]);
+} if (i == ciudadesDisponibles.length)
+    console.log("No hay pasajes disponibles");
+else
+    console.log("Puedes comprar pasaje para: " + ciudadesDisponibles[i]);
